0662f6d72cd1be7a8eec5229ba91e8ba5c55c157
Joseph Strauss <jospeh.strauss@gmail.com>
2015-02-16 12:52:42 -0500

    Droning about GitHub
    Here we watched a 30min tutorial about Computer spyware ;)

---
+++ SQL scripts/Class_01.sql
@@ -0,0 +1,11 @@
+﻿create table people(
+	name		text,
+	favoriteColor	text,
+	favoriteNumber	int
+)
+
+insert into people (name, favoriteColor, favoriteNumber)
+		value('Alan', 'blue', 007)
+
+select *
+from people
