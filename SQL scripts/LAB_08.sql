28706541251c262d821c04f9b0afb861cca36b13
Joseph Strauss <jospeh.strauss@gmail.com>
2015-04-02 12:43:52 -0400

    LAB 08 - suppliers Table
    Suppliers table is working properly

---
+++ SQL scripts/LAB_08.sql
@@ -0,0 +1,21 @@
+﻿--  LAB 8 Tables
+drop TABLE Suppliers;
+
+-- Suppliers --
+CREATE TABLE Suppliers(
+  name	   VARCHAR(15) not null,
+  addr 	   text,
+  city     text,
+  state	   CHAR(2),
+  zip	   CHAR(5),
+  contact  text,
+  payment  text,
+  primary key(name)
+);
+
+INSERT INTO Suppliers(name, addr, city, state, zip, contact, payment)
+  VALUES('KingdomTech', '294 Hooker Ave', 'Poughkeepsie', 'NY', 12603, 'Joe Strauss', 'VISA - Due at 15 of Month');
+
+select *
+from Suppliers;
+
